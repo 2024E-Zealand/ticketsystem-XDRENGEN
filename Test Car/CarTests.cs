26e1a3b06fc417f67d3042986819fefe88e42469
Christian Lund Nielsen <chn002@edu.zealand.dk>
2024-09-29 10:40:59 +0200

    Opgave 1 og 2

---
+++ Test Car/CarTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketClassLibrary.Tests
+{
+    [TestClass()]
+    public class CarTest
+    {
+        [TestMethod]
+        public void Price_ReturnsFixedPrice()
+        {
+            // Arrange
+            var car = new Car();
+
+            // Act
+            var result = car.Price();
+
+            // Assert
+            Assert.AreEqual(240, result);
+        }
+
+
+        [TestMethod]
+        public void VehicleType_ReturnsCar()
+        {
+            // Arrange
+            var car = new Car();
+
+            // Act
+            var result = car.VehicleType();
+
+            // Assert
+            Assert.AreEqual("Car", result);
+        }
+    }
+}
---
+++ TicketClassLibrary/Car.cs
@@ -0,0 +1,48 @@
+﻿namespace TicketClassLibrary
+{
+    /// <summary>
+    /// Beskriver en bil med en licenseplate og date.
+    /// </summary>
+    public class Car
+    {
+        /// <summary>
+        /// Bilens nummerplade.
+        /// </summary>
+        public string _licenseplate;
+
+        /// <summary>
+        /// Dato, f.eks. dato for overfart af bro.
+        /// </summary>
+        public DateTime _date;
+
+
+        /// <summary>
+        /// Henter eller sætter bilens nummerplade.
+        /// </summary>
+        public string Licenseplate { get; set; }
+
+        /// <summary>
+        /// Henter eller sætter datoen.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+
+        /// <summary>
+        /// Returnerer bilens faste pris.
+        /// </summary>
+        /// <returns>Den faste pris på 240.</returns>
+        public double Price()
+        {
+            return 240;
+        }
+
+        /// <summary>
+        /// Returnerer typen af bil.
+        /// </summary>
+        /// <returns>Strengen "Car"</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
